ba2c1e6271f4b5f83f975b0e57caa9721c7bf594
DOOM Gestor de Releases <doom.releases@latveria.org>
2026-03-09 22:21:42 +0100

    fix: migrar tiles de mapa a CartoDB Positron (neutre, sense banderes)
    - Canviat proveidor de tiles: openstreetmap.org -> basemaps.cartocdn.com/light_all
    - Estil net i neutre, adequat per projecte religios
    - Dades segueixen sent OpenStreetMap (comunitat oberta)
    - Gratuit, sense limits restrictius

--- src/components/MapaParroquias.tsx
+++ src/components/MapaParroquias.tsx
@@ -111,8 +111,10 @@ export default function MapaParroquias({
           className="h-full w-full"
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            subdomains="abcd"
+            maxZoom={20}
           />
 
           <CentrarEnSeleccion seleccionada={seleccionada} />
